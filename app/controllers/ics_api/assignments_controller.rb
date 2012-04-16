6e67d80ca7dc0c27369973665fc4945025ec8d35
Daniel Steiner <daniel.steiner@swisscom.com>
2012-04-16 16:48:56 +0200

    Controller erweitert
    Der Controller wurde erweitert. Der Controller ist nun im stande Parameter aus der URL auszulesen und die für den Benutzer bestimmten Assignments an die Instanzvariabel zu übergeben.
    
    Der Name des Helpers wurde gewechselt, da ich Ihn fälschlicherweise geändert habe.
    Die Version des Gems wurde angepasst.

--- app/controllers/ics_api/assignments_controller.rb
+++ app/controllers/ics_api/assignments_controller.rb
@@ -1,8 +1,11 @@
 class IcsApi::AssignmentsController < ApplicationController
-  def index
-        @calendar_event = Assignment.find(:all)
-      end
-    end
-  end
+  before_filter :get_current_user
   
+  def index
+    #Variable calendar_event get filled with dates from databasetable assignment
+    if params[:token] == current_user.icalendar_token
+        @calendar_event = current_user.assignments
+      end
+
+
 end
--- app/helpers/ics_api/assignments_helper.rb
+++ app/helpers/ics_api/assignments_helper.rb
@@ -1,5 +1,7 @@
 module AssignmentsHelper
+  # helper to convert the datetime format into an iCalendar file compatible version
   def calendar_time(time)
+      # this function strftime gets the new datetimeformation as a parameter.
     time.strftime("%Y%m%dT%H%M%SZ")
   end
 end
