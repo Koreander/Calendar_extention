da87ee8db9b32905cdf2bafd90bbad964f59e8e4
Daniel Steiner <daniel.steiner@swisscom.com>
2012-04-17 13:42:29 +0200

    Fertiges Gem
    Die letzte Änderung am Gem Controller wurde vorgenommen.
    Version um das Gem zu verpacken wurde geändert.

--- app/controllers/ics_api/assignments_controller.rb
+++ app/controllers/ics_api/assignments_controller.rb
@@ -2,8 +2,9 @@ class IcsApi::AssignmentsController < ApplicationController
   before_filter :get_current_user
   
   def index
-    #Variable calendar_event get filled with dates from databasetable assignment
-    if params[:token] == current_user.icalendar_token
+        #Reads out the url-parameters and uses them to discover the user
+    if params[:token] == current_user.icalendar_token  #??? michi fröge, möglich wenn benutzer nicht eingelogt ist
+         #Variable calendar_event get filled with dates from databasetable assignment
         @calendar_event = current_user.assignments
       end
 
--- lib/calendar_ext/version.rb
+++ lib/calendar_ext/version.rb
@@ -1,3 +1,3 @@
 module CalendarExt
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
